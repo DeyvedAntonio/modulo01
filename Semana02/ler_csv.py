1e42e190e4150b832250f4439011ca31b843d99a
Deyved Antonio <deyved.antonio@gmail.com>
2026-01-09 18:10:30 -0300

    feat: usar função para ler dados.

--- Semana02/ler_csv.py
+++ Semana02/ler_csv.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
 
-dados = pd.read_csv('dados.csv')
-print(dados)
+def carregar_dados(caminho):
+    dados = pd.read_csv(caminho)
+    return dados
+
+
+novos_dados = carregar_dados('dados.csv')
+print(novos_dados)
